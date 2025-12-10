f08daf1a8387df21939fb6ed4793f5de7f8f5454
vincent-guizot <vincentguizot128@gmail.com>
2025-12-10 14:00:37 +0700

    1

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
-module.exports = {
+// postcss.config.js
+import tailwindcss from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
+
+export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss,
+    autoprefixer,
   },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,6 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: { extend: {} },
-  plugins: [require("daisyui")], // kita tambahkan DaisyUI
-  daisyui: {
-    themes: ["light", "dark", "cupcake"], // bisa custom theme nanti
-  },
+  plugins: [require("daisyui")],
+  daisyui: { themes: ["light", "dark", "cupcake"] },
 };
